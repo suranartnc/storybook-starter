d186c9f61ee35d4690be6e6433df3f22c0a21a3d
SuranartN <suranartnc@gmail.com>
2017-03-03 11:29:22 +0700

    load stories dynamically

---
+++ src/components/Footer/Footer.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import Footer from './Footer';
+
+storiesOf('Footer', module)
+  .add('Normal', () => (
+    <Footer />
+  ));
