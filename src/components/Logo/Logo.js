767c4ef2c9ff86346a6e1a37f3b0ed49bcf6d78b
SuranartN <suranartnc@gmail.com>
2017-03-03 11:58:24 +0700

    add Logo component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../images/logo.svg'
+
+function Logo () {
+  return (
+    <div>
+      <img src={logo} />
+    </div>
+  )
+}
+
+export default Logo
