d29abafbb07860116f14cf3fcbe65884e48b3744
SuranartN <suranartnc@gmail.com>
2017-03-03 12:24:01 +0700

    sort storiesOf by prefix

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,8 +2,39 @@ import { configure } from '@kadira/storybook';
 
 const req = require.context('../src/components', true, /.stories.js$/)
 
+function sanitize(filename) {
+  filename = filename.replace('.stories.js', '').split('/').pop()
+  return filename
+}
+
+function sortByStoriesOf (components = []) {
+  let reorderComponents = []
+
+  components.forEach((component, index) => {
+    reorderComponents[index] = sanitize(component)
+  })
+  
+  reorderComponents.sort()
+
+  reorderComponents.forEach((name, index) => {
+    components.forEach((component) => {
+      if (component.indexOf(name) != -1) {
+        reorderComponents[index] = component
+      }
+    })
+  })
+
+  return reorderComponents
+}
+
 function loadStories() {
-  req.keys().forEach((filename) => req(filename))
+
+  const components = req.keys()
+  const reorderComponents = sortByStoriesOf(components)
+
+  reorderComponents.forEach((filename) => {
+    return req(filename)
+  })
 }
 
 configure(loadStories, module);
