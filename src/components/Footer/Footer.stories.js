a8c44cabd64d963676e6baffd0c3e342efd565dc
SuranartN <suranartnc@gmail.com>
2017-03-03 11:31:30 +0700

    add decorator styles

--- src/components/Footer/Footer.stories.js
+++ src/components/Footer/Footer.stories.js
@@ -2,7 +2,20 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import Footer from './Footer';
 
+const decorator = {
+  width: '100%',
+  maxWidth: '960px',
+  margin: '0 auto',
+  textAlign: 'center',
+  border: '1px solid gray'
+}
+
 storiesOf('Footer', module)
+  .addDecorator((story) => (
+    <div style={decorator}>
+      {story()}
+    </div>
+  ))
   .add('Normal', () => (
     <Footer />
   ));
